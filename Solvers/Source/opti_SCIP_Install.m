33ea8a7160a3724aaa2e6eddf3775d155b7f1e60
Jonathan Currie <jonathancurrie@users.noreply.github.com>
2017-06-24 14:48:45 +1200

    Updated SCIP install script for v4

--- Solvers/Source/opti_SCIP_Install.m
+++ Solvers/Source/opti_SCIP_Install.m
@@ -28,9 +28,9 @@
 % %% Visual Studio Builder Commands [LATEST SCIP REQ C++11]
 % %NOTE  - the PaxHeader folders change - update as required
 % clear
-% path = 'C:\Solvers\scipoptsuite-3.2.1\scip-3.2.1'; %e.g. 'C:\Solvers\SCIP'
-% splxpath = 'C:\Solvers\scipoptsuite-3.2.1\soplex-2.2.1'; %e.g. 'C:\Solvers\SOPLEX'
-% ipoptpath = 'C:\Solvers\Ipopt-3.12.6\Ipopt'; %e.g. 'C:\Solvers\IPOPT'
+% path = 'C:\Solvers\scipoptsuite-4.0.0\scip-4.0.0'; %e.g. 'C:\Solvers\SCIP'
+% splxpath = 'C:\Solvers\scipoptsuite-4.0.0\soplex-3.0.0'; %e.g. 'C:\Solvers\SOPLEX'
+% ipoptpath = 'C:\Solvers\Ipopt-3.12.7\Ipopt'; %e.g. 'C:\Solvers\IPOPT'
 % n = 1;
 % % SCIP
 % sdir = [path '\src'];
@@ -39,13 +39,14 @@
 % name = 'libscip';
 % opts = [];
 % opts.exPP = {'IPOPT_BUILD','_CRT_SECURE_NO_WARNINGS','NO_RAND_R','NO_SIGACTION','NO_STRERROR_R',...
-%              'NO_STRTOK_R','ROUNDING_MS','NPARASCIP','WITH_SCIPDEF'};          
+%              'NO_STRTOK_R','ROUNDING_MS','NPARASCIP','WITH_SCIPDEF','TPI_TNYC','SCIP_BUILDFLAGS="OPTI_BUILD"'};          
 % opts.exclude = {'exprinterpret_none.c','nlpi_ipopt_dummy.c','nlpi_xyz.c','lpi_none.c',...
 %                 'lpi_clp.cpp','lpi_cpx.c','lpi_grb.c','lpi_msk.c','lpi_qso.c',...
 %                 'lpi_spx.cpp','lpi_xprs.c','sorttpl.c','cmain.c',... %check whether we want lpi_spx2 or spx...?
 %                 'cppmain.cpp','disp_xyz.c','branch_xyz.c','event_xyz.c','cons_xyz.c',...
 %                 'heur_xyz.c','presol_xyz.c','prop_xyz.c','pricer_xyz.c','nodesel_xyz.c',...
-%                 'relax_xyz.c','reader_xyz.c','sepa_xyz.c','dialog_xyz.c','compr_xyz.c'};           
+%                 'relax_xyz.c','reader_xyz.c','sepa_xyz.c','dialog_xyz.c','compr_xyz.c',...
+%                 'tpi_none.c', 'tpi_openmp.c'};           
 % VSPRJ(n).sdir = sdir; VSPRJ(n).hdrs = hdrs; VSPRJ(n).name=name; VSPRJ(n).opts=opts; n = n + 1;
 % % SOPLEX
 % sdir = [splxpath '\src'];
@@ -60,7 +61,7 @@
 % name = 'scip';
 % opts = [];
 % opts.exPP = {'IPOPT_BUILD','_CRT_SECURE_NO_WARNINGS','NO_RAND_R','NO_SIGACTION','NO_STRERROR_R',...
-%              'NO_STRTOK_R','ROUNDING_MS','NPARASCIP'};
+%              'NO_STRTOK_R','ROUNDING_MS','NPARASCIP','TPI_OMP'};
 % opts.include = {'cppmain.cpp'};
 % opts.console = true;
 % opts.linklib = {'libscip.lib','libsoplex.lib','libipoptbm.lib','libdmumps_c.lib','libdmumps_f.lib','libseq_c.lib','libseq_f.lib','libmetis.lib','libpord.lib','libma57.lib'};
@@ -72,7 +73,7 @@
 % %%
 
 % Once complete, you will have a directory called SCIP/libscip. Open the
-% Visual Studio 2012 solution file, then complete the following steps:
+% Visual Studio solution file, then complete the following steps:
 %   a) (SCIP) Due to the way we have created the project the header files 
 %   cannot be found. Perform a find and replace on the CURRENT PROJECT of the
 %   following terms:
@@ -82,21 +83,24 @@
 %       - FIND #include "scip/          REPLACE #include "../scip/
 %       - FIND #include "nlpi/          REPLACE #include "../nlpi/
 %       - FIND #include "lpi/           REPLACE #include "../lpi/
+%       - FIND #include "tpi/           REPLACE #include "../tpi/
 %       - FIND #include "objscip/       REPLACE #include "../objscip/
 %       - FIND #include "xml/           REPLACE #include "../xml/
+%       - FIND #include "tinycthread/   REPLACE #include "../tinycthread/
 %       - FIND #include "dijkstra/      REPLACE #include "../dijkstra/
 %       - FIND #include <cppad/         REPLACE #include <../cppad/
 %       - FIND # include <cppad/        REPLACE # include <../cppad/
-%   b) In src/cppad/configure.hpp for 32bit change lines 120 and 134
-%           # define CPPAD_SIZE_T_SAME_UNSIGNED_INT 1
+%   b) In src/cppad/configure.hpp for 32bit change lines 150 and 164
+%           # define CPPAD_SIZE_T_NOT_UNSIGNED_INT 0
 %           # define CPPAD_TAPE_ADDR_TYPE size_t
 %      while for 64bit change it to
-%           # define CPPAD_SIZE_T_SAME_UNSIGNED_INT 0
+%           # define CPPAD_SIZE_T_NOT_UNSIGNED_INT 1
 %           # define CPPAD_TAPE_ADDR_TYPE size_t
 %   c) You MAY need to change line 2789 of nlpi_ipopt.cpp to "#ifdef F77_FUNC"
 %      if you encounter linker errors around "SCIPsolveLinearProb".
-%   d) Build a Win32 or x64 Release of each project to compile the code.
-%   e) Copy the generated .lib files to the following folder:
+%   d) Comment #include "buildflags.c" from scipbuildflags.c line 24
+%   e) Build a Win32 or x64 Release of each project to compile the code.
+%   f) Copy the generated .lib files to the following folder:
 %
 %   OPTI/Solvers/scip/Source/lib/win32 or win64
 %
