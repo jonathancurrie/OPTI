aacb94596ba9f01b85028b6f49c21480816228d2
Jonathan Currie <jonathancurrie@users.noreply.github.com>
2017-07-10 20:23:56 +1200

    Update to contents for release.

--- Utilities/opti/Contents.m
+++ Utilities/opti/Contents.m
@@ -1,4 +1,4 @@
 % OPTI Toolbox
-% Version 2.27 (R2013a) 10-Jul-2017
+% Version 2.27 (R2017a) 10-Jul-2017
 % Copyright (C) 2011-2017 Jonathan Currie (Inverse Problem Ltd)
 % License: https://inverseproblem.co.nz/OPTI/index.php/DL/License
