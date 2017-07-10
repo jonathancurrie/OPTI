c2c2bb03e5406e5a2aee619b9f6ec2c7dcfe21c1
Jonathan Currie <jonathancurrie@users.noreply.github.com>
2017-07-10 20:00:40 +1200

    Added try catch around dist test

--- Utilities/Install/opti_Dist_Test.m
+++ Utilities/Install/opti_Dist_Test.m
@@ -3,120 +3,128 @@
 clc
 clear all
 
-%Run Install Test
-opti_Install_Test;
+addpath([cd filesep 'Test Problems/Development']);
+try
 
-%Check MEX Interfaces all working
-vers = optiSolver('ver');
+    %Run Install Test
+    opti_Install_Test;
 
-%Run Benchmarks
-optiBench('LP');
-optiBench('BILP');
-optiBench('MILP');
-optiBench('QP');
-optiBench('SDP');
-optiBench('NLS');
-optiBench('NLP');
+    %Check MEX Interfaces all working
+    vers = optiSolver('ver');
 
-%Run Demos
-Basic_Usage;
-MATLAB_Overloads;
-LinearProgramming;
-MixedInteger_LinearProgramming;
-QuadraticProgramming;
-Global_NonlinearProgramming;
-NonlinearProgramming;
-Differentiation_Examples;
-FileIO_Examples;
-AMPL_Examples;
+    %Run Benchmarks
+    optiBench('LP');
+    optiBench('BILP');
+    optiBench('MILP');
+    optiBench('QP');
+    optiBench('SDP');
+    optiBench('NLS');
+    optiBench('NLP');
 
-%Run Wiki Code
-opti_wikicode;
+    %Run Demos
+    Basic_Usage;
+    MATLAB_Overloads;
+    LinearProgramming;
+    MixedInteger_LinearProgramming;
+    QuadraticProgramming;
+    Global_NonlinearProgramming;
+    NonlinearProgramming;
+    Differentiation_Examples;
+    FileIO_Examples;
+    AMPL_Examples;
 
-%Run MATLAB Wrapper Soak Tests
-test_probs_mw;
-test_probs_mw;
-test_probs_mw;
+    %Run Wiki Code
+    opti_wikicode;
 
-%Run MPS Test
-test_mps;
+    %Run MATLAB Wrapper Soak Tests
+    test_probs_mw;
+    test_probs_mw;
+    test_probs_mw;
 
-%Run SOS Test
-test_sos;
+    %Run MPS Test
+    test_mps;
 
-%QC Rest
-test_new_qc;
+    %Run SOS Test
+    test_sos;
 
-%Run Row Bounds Test
-test_rowcon
+    %QC Rest
+    test_new_qc;
 
-%Run NumDiff Test
-test_numdiff;
+    %Run Row Bounds Test
+    test_rowcon
 
-%Run Global Test Set
-test_global;
+    %Run NumDiff Test
+    test_numdiff;
 
-%Run iterfun Test
-test_iterfunc;
+    %Run Global Test Set
+    test_global;
 
-%Run hessian Test
-test_hessian;
+    %Run iterfun Test
+    test_iterfunc;
 
-%Run AMPL Soak Tests
-test_ampl;
-test_ampl;
-test_ampl;
+    %Run hessian Test
+    test_hessian;
 
-%Run Solver Tests
-test_clp;
-test_cbc;
-test_filterSD;
-test_nlopt;
-test_ooqp;
-test_scip_formal;
+    %Run AMPL Soak Tests
+    test_ampl;
+    test_ampl;
+    test_ampl;
 
-%Run linear constraint tests
-test_ipopt_linearcon;
-test_bonmin_linearcon;
+    %Run Solver Tests
+    test_clp;
+    test_cbc;
+    test_filterSD;
+    test_nlopt;
+    test_ooqp;
+    test_scip_formal;
 
-%Run sdp tests
-test_csdp;
-test_dsdp;
-test_sedumi;
-test_sdpio;
+    %Run linear constraint tests
+    test_ipopt_linearcon;
+    test_bonmin_linearcon;
 
-%Run other tests
-test_objc;
-test_sym_diff;
-test_sparse_snle;
-test_rmathlib_formal;
+    %Run sdp tests
+    test_csdp;
+    test_dsdp;
+    test_sedumi;
+    test_sdpio;
 
-%Plot tests
-test_1dplots;
-test_3dplots;
+    %Run other tests
+    test_objc;
+    test_sym_diff;
+    test_sparse_snle;
+    test_rmathlib_formal;
 
-%Multisolve Tests
-test_multisolve;
-auto2fit_problems;
+    %Plot tests
+    test_1dplots;
+    test_3dplots;
 
-%Confidence Tests
-test_conf_formal;
-test_conf;
-test_optifit;
-test_nist_fit;
+    %Multisolve Tests
+    test_multisolve;
+    auto2fit_problems;
 
-%Dynamic Optimization Tests
-test_dyn_nls;
-dnls_examples;
+    %Confidence Tests
+    test_conf_formal;
+    test_conf;
+    test_optifit;
+    test_nist_fit;
 
-%Run General Test Set
-test_probs;
+    %Dynamic Optimization Tests
+    test_dyn_nls;
+    dnls_examples;
 
-%Run opti_debug then delete output file
-opti_debug;
-pause(0.1); rehash;
-delete('opti_debug.txt');
+    %Run General Test Set
+    test_probs;
 
-clc
-opti_package;
-fprintf('\nAll Appears OK!\n\n');
+    %Run opti_debug then delete output file
+    opti_debug;
+    pause(0.1); rehash;
+    delete('opti_debug.txt');
+
+    clc
+    opti_package;
+    rmpath([cd filesep 'Test Problems/Development']);
+    fprintf('\nAll Appears OK!\n\n');
+catch ME
+    rmpath([cd filesep 'Test Problems/Development']);
+    rethrow(ME);
+end
